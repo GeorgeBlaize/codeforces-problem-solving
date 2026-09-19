1cead57584d699728da8b010602d548072fa3c57
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2026-09-19 23:41:05 +0600

    Add files via upload

---
+++ 800/A_Presents.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{   
+
+    int n;
+    
+    cin>>n;
+
+    int ans[n+2];
+
+    for(int i=1; i<=n; i++){
+
+        int x;
+
+        cin>>x;
+
+        ans[x]=i;
+
+
+    }
+
+    for(int i=1; i<=n; i++){
+
+        cout<<ans[i]<<" ";
+        
+    }
+
+    return 0;
+}
